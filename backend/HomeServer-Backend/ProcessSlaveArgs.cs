3e2ecca948706a242e3d8f3bb2354fd6d59af392
Nadav Mordechai <nadav26740@gmail.com>
2025-07-12 11:46:25 +0300

    SlaveArgs class added to contain info for saving and loading processes

---
+++ backend/HomeServer-Backend/ProcessSlaveArgs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeServer_Backend
+{
+    public class ProcessSlaveArgs
+    {
+        public ProcessesManager.ProcessPriority Priority { get; set; }
+        public ProcessHandler.ProcessInfo ProcessInfo { get; set; }
+        public bool AutoStart { get; set; } // If true, the process will be started automatically if it is not running
+
+        public ProcessesManager.ProcessSlave CreateProcessSlave() => new ProcessesManager.ProcessSlave(new ProcessHandler(ProcessInfo), AutoStart, Priority);
+        
+        // fast deserilize to json
+        public string DeserilizeToJson()
+        {
+            return $"{{{nameof(Priority)}:{Priority}, {nameof(ProcessInfo)}:TODO, {nameof(AutoStart)}: {AutoStart}}}";
+        }
+    }
+}
