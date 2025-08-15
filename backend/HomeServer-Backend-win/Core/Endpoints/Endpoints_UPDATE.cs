07fee671d83673f14680e671a3773aefbce31685
Nadav Mordechai <nadav26740@gmail.com>
2025-08-15 19:07:45 +0300

    endpoints and routing splited between files

---
+++ backend/HomeServer-Backend-win/Core/Endpoints/Endpoints_UPDATE.cs
@@ -0,0 +1,78 @@
+﻿using HomeServer_Backend.Communication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeServer_Backend.Core
+{
+    public partial class ServerCore
+    {
+        /// <summary>
+        /// Trying to start process under the given tag
+        /// </summary>
+        /// <param name="data">process tag</param>
+        /// <returns>200 if successed to stop or error code if failed</returns>
+        private ServerMessageFormat ApiProcessesStart(string data)
+        {
+
+            ProcessesManager.ProcessSlave? slave = this.m_Manager.FindProcess(data);
+            if (slave == null)
+            {
+                Logger.LogError($"[ApiProcessesStart] Process {data} not found.");
+                return new ServerMessageFormat { Data = "Process not found", StatusCode = 404 };
+            }
+
+            try
+            {
+                slave.ProcessHandler.StartProcess();
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError($"[ApiProcessesStart] Error while starting process {data}: {ex.Message}");
+                return new ServerMessageFormat { Data = $"Error while starting process: {ex.Message}", StatusCode = 500 };
+            }
+
+            Logger.LogInfo($"[ApiProcessesStart] Process {data} started successfully.");
+            return new ServerMessageFormat
+            {
+                Data = $"Process {data} started successfully.",
+                StatusCode = 200
+            };
+        }
+
+        /// <summary>
+        /// Trying to stop process under the given tag
+        /// </summary>
+        /// <param name="data">Processes tag</param>
+        /// <returns>200 if sucessed or error message</returns>
+        private ServerMessageFormat ApiProcessesStop(string data)
+        {
+            ProcessesManager.ProcessSlave? slave = this.m_Manager.FindProcess(data);
+            if (slave == null)
+            {
+                Logger.LogError($"[ApiProcessesStart] Process {data} not found.");
+                return new ServerMessageFormat { Data = "Process not found", StatusCode = 404 };
+            }
+
+            try
+            {
+                slave.ProcessHandler.StopProcess();
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError($"[ApiProcessesStart] Error while stopping process {data}: {ex.Message}");
+                return new ServerMessageFormat { Data = $"Error while stopping process: {ex.Message}", StatusCode = 500 };
+            }
+
+            Logger.LogInfo($"[ApiProcessesStart] Process {data} stopped successfully.");
+            return new ServerMessageFormat
+            {
+                Data = $"Process {data} stopped successfully.",
+                StatusCode = 200
+            };
+        }
+
+    }
+}
---
+++ backend/HomeServer-Backend-win/Core/ServerCoreRouting.cs
@@ -0,0 +1,103 @@
+﻿using HomeServer_Backend.Communication;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeServer_Backend.Core
+{
+    public partial class ServerCore
+    {
+        // ============ Handle Types ===============
+
+        /// <summary>
+        /// Routing GET requests to the appropriate handlers.
+        /// </summary>
+        /// <param name="message">Client message</param>
+        /// <returns>Server response</returns>
+        private ServerMessageFormat HandleGETRequests(ClientMessageFormat message)
+        {
+            
+            switch (message.Path.ToLower())
+            {
+                case "/api/processes":
+                    // Return all processes status
+                    return ApiProcesses(message.Data);
+
+                case "/api/processes/status":
+                    return ApiProcessesStatus(message.Data);
+
+                case "/api/process/lastlogs":
+                    return ApiProcessLastLogs(message.Data);
+
+                case "/api/process/lasterrors":
+                    return ApiProcessLastErrors(message.Data);
+
+                default:
+                    // Handle other GET requests
+                    Logger.LogError($"Unknown GET request path: {message.Path} with data: {message.Data}");
+                    return new() { Data = "Unknown GET request path", StatusCode = 404 };
+            }
+        }
+
+        /// <summary>
+        /// Routing POST requests to the appropriate handlers.
+        /// </summary>
+        /// <param name="message">Client message</param>
+        /// <returns>Server response</returns>
+        private ServerMessageFormat HandlePOSTRequests(ClientMessageFormat message)
+        {
+            switch (message.Path.ToLower())
+            {
+                case "api/process/input":
+                    return ApiProcessInput(message.Data);
+
+                default:
+                    // Handle other GET requests
+                    Logger.LogError($"Unknown POST request path: {message.Path} with data: {message.Data}");
+                    return new() { Data = "Unknown POST request path", StatusCode = 404 };
+            }
+        }
+
+        /// <summary>
+        /// Routing DELETE requests to the appropriate handlers.
+        /// </summary>
+        /// <param name="message">Client message</param>
+        /// <returns>Server response</returns>
+        private ServerMessageFormat HandleDELETERequests(ClientMessageFormat message)
+        {
+            switch (message.Path.ToLower())
+            {
+                default:
+                    // Handle other GET requests
+                    Logger.LogError($"Unknown DELETE request path: {message.Path} with data: {message.Data}");
+                    return new() { Data = "Unknown DELETE request path", StatusCode = 404 };
+            }
+        }
+
+        /// <summary>
+        /// Routing UPDATE requests to the appropriate handlers.
+        /// </summary>
+        /// <param name="message">Client message</param>
+        /// <returns>Server response</returns>
+        private ServerMessageFormat HandleUPDATERequests(ClientMessageFormat message)
+        {
+            switch (message.Path.ToLower())
+            {
+                case "/api/process/start":
+                    return ApiProcessesStart(message.Data);
+
+                case "/api/process/stop":
+                    return ApiProcessesStop(message.Data);
+
+                default:
+                    // Handle other GET requests
+                    Logger.LogError($"Unknown UPDATE request path: {message.Path} with data: {message.Data}");
+                    return new() { Data = "Unknown UPDATE request path", StatusCode = 404 };
+            }
+        }
+    }
+}
